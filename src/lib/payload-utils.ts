2b63baabcf54a912909352f60ab1d8d7040d629c
MinhOmega <quang.minh@snaptec.co>
2024-04-10 22:59:38 +0700

    update code

--- src/lib/payload-utils.ts
+++ src/lib/payload-utils.ts
@@ -1,24 +1,17 @@
-import { User } from '../payload-types'
-import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies'
-import { NextRequest } from 'next/server'
+import { User } from "../payload-types";
+import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
+import { NextRequest } from "next/server";
 
-export const getServerSideUser = async (
-  cookies: NextRequest['cookies'] | ReadonlyRequestCookies
-) => {
-  const token = cookies.get('payload-token')?.value
-
-  const meRes = await fetch(
-    `${process.env.SERVER_URL}/api/users/me`,
-    {
-      headers: {
-        Authorization: `JWT ${token}`,
-      },
-    }
-  )
-
+export const getServerSideUser = async (cookies: NextRequest["cookies"] | ReadonlyRequestCookies) => {
+  const token = cookies.get("payload-token")?.value;
+  if (!token) return { user: null };
+  const meRes = await fetch(`${process.env.SERVER_URL}/api/users/me`, {
+    headers: {
+      Authorization: `JWT ${token}`,
+    },
+  });
   const { user } = (await meRes.json()) as {
-    user: User | null
-  }
-
-  return { user }
-}
+    user: User | null;
+  };
+  return { user };
+};
