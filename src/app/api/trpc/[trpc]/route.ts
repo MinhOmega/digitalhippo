57aa2591a123ba47672ecacdfea94a1427aae2ba
MinhOmega <quang.minh@snaptec.co>
2024-04-11 00:47:24 +0700

    update code

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,14 +1,13 @@
-import { appRouter } from '@/trpc'
-import { fetchRequestHandler } from '@trpc/server/adapters/fetch'
+import { appRouter } from "@/trpc";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { NextRequest } from "next/server";
 
-const handler = (req: Request) => {
-  fetchRequestHandler({
-    endpoint: '/api/trpc',
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
     req,
     router: appRouter,
     // @ts-expect-error context already passed from express middleware
     createContext: () => ({}),
-  })
+  });
 }
-
-export { handler as GET, handler as POST }
